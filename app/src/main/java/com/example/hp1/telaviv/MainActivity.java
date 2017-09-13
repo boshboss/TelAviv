728c2ec89d9cafe4f792ff9fa3bb3c27b24a947c
boshboss <boshboss74@gmail.com>
2017-09-13 08:56:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hp1/telaviv/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.hp1.telaviv;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+Button btcourse,btinfo,btregister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btcourse=(Button)findViewById(R.id.btcourse);
+        btinfo=(Button)findViewById(R.id.btinfo);
+        btregister=(Button)findViewById(R.id.btregister);
+        btcourse.setOnClickListener(this);
+        btregister.setOnClickListener(this);
+        btinfo.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==btcourse){
+            Intent i=new Intent(this,courses.class);
+            startActivity(i);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hp1/telaviv/courses.java
@@ -0,0 +1,38 @@
+package com.example.hp1.telaviv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class courses extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ListView lv1;
+    ArrayList<String> courses= new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses);
+        lv1= (ListView) findViewById(R.id.lv1);
+        courses.add(new String("Computers"));
+        courses.add(new String("Doctor"));
+        courses.add(new String("trash man"));
+        courses.add(new String("teacher"));
+
+
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,courses);
+        lv1.setAdapter(adapter);
+
+        lv1.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+}
