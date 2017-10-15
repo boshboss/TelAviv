d8892d2b9d4bd028cb8c8e471200f41d7d80d456
ananc <anan.cena3@gmail.com>
2017-10-15 13:00:20 +0100

    ready llemte7an

--- app/src/main/java/com/example/hp1/telaviv/MainActivity.java
+++ app/src/main/java/com/example/hp1/telaviv/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 Button btcourse,btinfo,btregister,btphone;
     @Override
+    //te3ud
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
